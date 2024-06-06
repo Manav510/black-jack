566649dea8fe23eae481481d729921c747ec5c20
Manav510 <98536459+Manav510@users.noreply.github.com>
2024-06-06 10:37:19 +0530

    exiting the result popup

--- src/BlackJack/Game.tsx
+++ src/BlackJack/Game.tsx
@@ -1,18 +1,34 @@
 import { Container } from "@pixi/react-animated";
 
 import GameContext from "./context";
-import { useContext } from "react";
+import { useContext, useEffect, useRef } from "react";
 import useGame from "./useGame";
 
 import StartGame from "./components/StartGame";
 import Dealer_cards from "./components/Dealer_cards";
 import PlayerTurn from "./components/PlayerTurn";
 import Popup from "./components/Popup";
+
 const GameComp = () => {
-  const { game_started,reset_game,show_popup,result } = useContext(GameContext);
-    const handleClosePopup = () => {
+  const { game_started, reset_game, show_popup, result } = useContext(GameContext);
+  const handleClosePopup = () => {
     reset_game();
   };
+
+  const timeoutRef = useRef(null);
+
+  useEffect(() => {
+    clearTimeout(timeoutRef.current); 
+
+    if (show_popup) {
+      timeoutRef.current = setTimeout(() => {
+        handleClosePopup();
+      }, 2000); 
+    }
+
+    return () => clearTimeout(timeoutRef.current); 
+  }, [show_popup, handleClosePopup]);
+
   return (
     <Container sortableChildren={true}>
       {!game_started && <StartGame />}
